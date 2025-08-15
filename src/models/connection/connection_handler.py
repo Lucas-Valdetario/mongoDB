6975261eaf5415f8f86065cb233130a625165a50
Lucas Valdetario Vieira <lucas.vieira@embrapii.org.br>
2025-08-15 17:52:03 -0300

    Updating

--- src/models/connection/connection_handler.py
+++ src/models/connection/connection_handler.py
@@ -2,4 +2,14 @@ from pymongo import MongoClient
 
 class DBConnectionHandler:
     def __init__(self) -> None:
-        self.__connection_string = 'mongodb://{}:{}@{}:{}/?authSource=admin'.format()
+        self.__connection_str = 'mongodb://localhost:27017'
+        self.__database_name = "rocket_db"
+        self.__client = None
+        self.__db_connection = None
+
+    def connect_to_db(self):
+        self.__client = MongoClient(self.__connection_str)
+        self.__db_connection = self.__client[self.__database_name]
+
+    def get_db_connection(self):
+        return self.__db_connection
---
+++ src/models/repository/repository_test.py
@@ -0,0 +1,11 @@
+from src.models.connection.connection_handler import DBConnectionHandler
+from .orders_repository import OrdersRepository
+
+db_connection_handler = DBConnectionHandler()
+db_connection_handler.connect_to_db()
+conn = db_connection_handler.get_db_connection()
+
+def test_insert_document():
+    orders_repository = OrdersRepository(conn)
+    my_doc = {"order_id": 1, "customer_name": "John Doe", "items": ["item1", "item2"], "total": 100.0}
+    orders_repository.insert_document(my_doc)
