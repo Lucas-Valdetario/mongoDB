beea6d93452753434b507db6bedcae41d93f3ce3
Lucas Valdetario Vieira <lucas.vieira@embrapii.org.br>
2025-08-18 12:32:37 -0300

    Updating

--- src/models/repository/repository_test.py
+++ src/models/repository/repository_test.py
@@ -9,3 +9,8 @@ def test_insert_document():
     orders_repository = OrdersRepository(conn)
     my_doc = {"order_id": 1, "customer_name": "John Doe", "items": ["item1", "item2"], "total": 100.0}
     orders_repository.insert_document(my_doc)
+
+def test_insert_list_of_documents():
+    orders_repository = OrdersRepository(conn)
+    my_doc = [{"elem1":"aqui"}, {"elem2": "aqui2"}, {"elem3": "aqui3"}]
+    orders_repository.insert_list_of_documents(my_doc)
